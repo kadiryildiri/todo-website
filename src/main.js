6c08a8710bab7bf36b69e2a71d3e38511d70f67b
Kadir Yıldırım <kadir.yildiri.71@gmail.com>
2025-02-14 02:23:02 +0300

    Add desktop UI

--- src/main.js
+++ src/main.js
@@ -50,15 +50,17 @@ const renderUI = (displayTodos = todos) => {
             let todoItem = document.createElement("div")
             todoItem.className = "flex items-center border-b-gray-700 border-b-1"
             let todoCheck = document.createElement("div")
-            todoCheck.className = "bg-[#25273C] h-12 w-16 flex items-center justify-center rounded-l-md"
+            todoCheck.className = "bg-[#25273C] h-12 w-16 flex items-center justify-center rounded-l-md md:h-18"
             let todoCheckSpan = document.createElement("span")
-            todoCheckSpan.className = "w-5 h-5 border-[#cacde8] rounded-full border-1 flex items-center justify-center"
+            todoCheckSpan.className =
+                "w-5 h-5 border-[#cacde8] hover:border-[#57ddff] transition-all  rounded-full border-1 flex items-center justify-center md:hover:cursor-pointer"
             let todoText = document.createElement("p")
-            todoText.className = "w-full p-3 text-white bg-[#25273C] font-josefin-sans"
+            todoText.className =
+                "w-full p-3 text-[#f5f5f5e7] bg-[#25273C] font-josefin-sans md:h-18 flex items-center md:text-xl"
             let todoCross = document.createElement("div")
-            todoCross.className = "bg-[#25273C] h-12 w-16 flex items-center justify-center rounded-r-md"
+            todoCross.className = "bg-[#25273C] h-12 w-16 flex items-center justify-center rounded-r-md md:h-18 "
             let crossBtn = document.createElement("button")
-            crossBtn.className = "deleteBtn"
+            crossBtn.className = "md:cursor-pointer"
             let crossImg = document.createElement("img")
             crossImg.src = "src/images/icon-cross.svg"
 
@@ -79,6 +81,33 @@ const renderUI = (displayTodos = todos) => {
                 todoCheckSpan.classList.add("bg-gradient-to-r", "from-[#57ddff]", "to-[#c058f3]")
                 todoText.classList.add("line-through")
             }
+
+            all.addEventListener("click", () => {
+                all.classList.remove("text-[#f5f5f579]")
+                all.classList.add("text-blue-400")
+                active.classList.remove("text-blue-400")
+                completed.classList.remove("text-blue-400")
+                active.classList.add("text-[#f5f5f579]")
+                completed.classList.add("text-[#f5f5f579]")
+            })
+
+            active.addEventListener("click", () => {
+                active.classList.remove("text-[#f5f5f579]")
+                active.classList.add("text-blue-400")
+                all.classList.remove("text-blue-400")
+                completed.classList.remove("text-blue-400")
+                all.classList.add("text-[#f5f5f579]")
+                completed.classList.add("text-[#f5f5f579]")
+            })
+
+            completed.addEventListener("click", () => {
+                completed.classList.remove("text-[#f5f5f579]")
+                completed.classList.add("text-blue-400")
+                active.classList.remove("text-blue-400")
+                all.classList.remove("text-blue-400")
+                active.classList.add("text-[#f5f5f579]")
+                all.classList.add("text-[#f5f5f579]")
+            })
 
             crossBtn.addEventListener("click", () => {
                 deleteItem(todo.id)
@@ -108,15 +137,17 @@ const renderUI = (displayTodos = todos) => {
             let todoItem = document.createElement("div")
             todoItem.className = "flex items-center border-b-gray-700 border-b-1"
             let todoCheck = document.createElement("div")
-            todoCheck.className = "flex items-center justify-center w-16 h-12 bg-white rounded-l-md"
+            todoCheck.className = "flex items-center justify-center w-16 h-12 bg-white rounded-l-md md:h-18"
             let todoCheckSpan = document.createElement("span")
-            todoCheckSpan.className = "w-5 h-5 border-[#cacde8] rounded-full border-1 flex items-center justify-center"
+            todoCheckSpan.className =
+                "w-5 h-5 border-[#cacde8] hover:border-[#57ddff] transition-all rounded-full border-1 flex items-center justify-center md:hover:cursor-pointer"
             let todoText = document.createElement("p")
-            todoText.className = "w-full p-3 text-[#25273C] bg-white font-josefin-sans"
+            todoText.className =
+                "w-full p-3 text-[#25273C] bg-white font-josefin-sans md:h-18 flex items-center md:text-xl"
             let todoCross = document.createElement("div")
-            todoCross.className = "flex items-center justify-center w-16 h-12 bg-white rounded-r-md"
+            todoCross.className = "flex items-center justify-center w-16 h-12 bg-white rounded-r-md md:h-18"
             let crossBtn = document.createElement("button")
-            crossBtn.className = "deleteBtn"
+            crossBtn.className = "md:cursor-pointer"
             let crossImg = document.createElement("img")
             crossImg.src = "src/images/icon-cross.svg"
 
@@ -138,6 +169,40 @@ const renderUI = (displayTodos = todos) => {
                 todoText.classList.add("line-through")
             }
 
+            all.addEventListener("click", () => {
+                all.classList.remove("text-[#f5f5f579]")
+                all.classList.add("text-blue-400")
+                active.classList.remove("text-[#f5f5f579]")
+                completed.classList.remove("text-[#f5f5f579]")
+                active.classList.remove("text-blue-400")
+                completed.classList.remove("text-blue-400")
+                active.classList.add("text-[#25273C]")
+                completed.classList.add("text-[#25273C]")
+                renderUI()
+            })
+
+            active.addEventListener("click", () => {
+                active.classList.remove("text-[#f5f5f579]")
+                active.classList.add("text-blue-400")
+                all.classList.remove("text-blue-400")
+                all.classList.remove("text-[#f5f5f579]")
+                completed.classList.remove("text-[#f5f5f579]")
+                completed.classList.remove("text-blue-400")
+                all.classList.add("text-[#25273C]")
+                completed.classList.add("text-[#25273C]")
+            })
+
+            completed.addEventListener("click", () => {
+                completed.classList.remove("text-[#f5f5f579]")
+                completed.classList.add("text-blue-400")
+                all.classList.remove("text-[#f5f5f579]")
+                active.classList.remove("text-[#f5f5f579]")
+                active.classList.remove("text-blue-400")
+                all.classList.remove("text-blue-400")
+                active.classList.add("text-[#25273C]")
+                all.classList.add("text-[#25273C]")
+            })
+
             crossBtn.addEventListener("click", () => {
                 deleteItem(todo.id)
             })
@@ -147,6 +212,7 @@ const renderUI = (displayTodos = todos) => {
                     checkImg.src = "src/images/icon-check.svg"
                     todoCheckSpan.appendChild(checkImg)
                     todoCheckSpan.classList.add("bg-linear-to-r", "from-[#57ddff]", "to-[#c058f3]")
+                    todoText.classList.remove("text-[#f5f5f5e7]")
                     todoText.classList.add("line-through")
                     todo.completed = !todo.completed
                     addToStorage()
@@ -209,12 +275,16 @@ theme.addEventListener("click", () => {
         input.classList.add("text-[#25273C]")
         info.classList.remove("bg-[#25273C]")
         info.classList.add("bg-[#ffffff]")
-        info.classList.remove("text-white")
+        info.classList.remove("text-[#f5f5f5e7]")
         info.classList.add("text-[#25273C]")
         settings.classList.remove("bg-[#25273C]")
         settings.classList.add("bg-[#ffffff]")
-        settings.classList.remove("text-white")
+        settings.classList.remove("text-[#f5f5f5e7]")
         settings.classList.add("text-[#25273C]")
+        active.classList.remove("text-[#f5f5f579]")
+        active.classList.add("text-[#25273C]")
+        completed.classList.remove("text-[#f5f5f579]")
+        completed.classList.add("text-[#25273C]")
         currentTheme = "light"
         renderUI()
     } else {
@@ -231,12 +301,16 @@ theme.addEventListener("click", () => {
         input.classList.remove("text-[#25273C]")
         info.classList.add("bg-[#25273C]")
         info.classList.remove("bg-[#ffffff]")
-        info.classList.add("text-white")
+        info.classList.add("text-[#f5f5f5e7]")
         info.classList.remove("text-[#25273C]")
         settings.classList.add("bg-[#25273C]")
         settings.classList.remove("bg-[#ffffff]")
-        settings.classList.add("text-white")
+        settings.classList.add("text-[#f5f5f5e7]")
         settings.classList.remove("text-[#25273C]")
+        active.classList.remove("text-[#25273C]")
+        active.classList.add("text-[#f5f5f579]")
+        completed.classList.remove("text-[#25273C]")
+        completed.classList.add("text-[#f5f5f579]")
         currentTheme = "dark"
         renderUI()
     }
